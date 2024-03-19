33b015765495f1e73c748a6b7661ad7bbcc6860f
hojun <ghwns9393@naver.com>
2024-03-19 12:31:42 +0900

    3rd push lhj

---
+++ hrms/src/main/java/edu/hrms/service/UserLoginSuccessHandler.java
@@ -0,0 +1,33 @@
+package edu.hrms.service;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import edu.hrms.vo.UserVO;
+
+public class UserLoginSuccessHandler implements AuthenticationSuccessHandler {
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+		
+		UserVO loginUser = (UserVO) authentication.getPrincipal();
+		
+		System.out.println(loginUser.getUserid());
+		
+		// 로그인 성공
+		System.out.println("로그인 성공!!");
+		response.sendRedirect(request.getContextPath());
+		
+	}
+
+	
+	
+	
+}
