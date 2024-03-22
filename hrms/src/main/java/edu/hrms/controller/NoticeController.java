3ab865f3e18e1dda91b3c49293fe582736ef8e4b
aura1120 <155043429+aura1120@users.noreply.github.com>
2024-03-22 09:34:50 +0900

    d

--- hrms/src/main/java/edu/hrms/controller/NoticeController.java
+++ hrms/src/main/java/edu/hrms/controller/NoticeController.java
@@ -1,13 +1,19 @@
 package edu.hrms.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import edu.hrms.service.NoticeService;
+
 @Controller
 @RequestMapping(value ="/notice")
 public class NoticeController {		
+	
+	@Autowired
+	NoticeService noticeService;
 	
 	@RequestMapping(value = "/main.do", method = RequestMethod.GET)
 	public String notice(Model model) {
@@ -15,4 +21,15 @@ public class NoticeController {
 		return "/notice/main";
 	}
 	
+	@RequestMapping(value = "/view.do", method = RequestMethod.GET)
+	public String view() {
+		
+		return "/notice/view";
+	}
+	
+	@RequestMapping(value = "/write.do", method = RequestMethod.GET)
+	public String write() {
+		
+		return "/notice/write";
+	}
 }
